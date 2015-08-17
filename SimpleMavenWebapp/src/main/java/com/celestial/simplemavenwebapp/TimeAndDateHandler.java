7b76c3c85f60948bc8ac780bc58a3972e049e1af
stream2stream <swright@celestial.co.uk>
2015-08-17 14:06:55 +0100

    A DevOp app

---
+++ SimpleMavenWebapp/src/main/java/com/celestial/simplemavenwebapp/TimeAndDateHandler.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.celestial.simplemavenwebapp;
+
+import java.sql.Time;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author selvy_000
+ */
+public class TimeAndDateHandler
+{
+    private Date    theDate;
+    private Time    theTime;
+    private long    milliSecs;
+    
+    public  String  getDate()
+    {
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        theDate = new Date();
+        System.out.println(dateFormat.format(theDate)); //2014/08/06 15:59:48
+        
+        return dateFormat.format(theDate);
+    }
+    
+    public  String  getTime()
+    {
+        DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+        theDate = new Date();
+        System.out.println(dateFormat.format(theDate)); //2014/08/06 15:59:48
+        
+        return dateFormat.format(theDate);
+    }
+    
+    static  public  void    main( String[] args )
+    {
+        TimeAndDateHandler tadh = new TimeAndDateHandler();
+        
+        tadh.getDate();
+        tadh.getTime();
+    }
+}
